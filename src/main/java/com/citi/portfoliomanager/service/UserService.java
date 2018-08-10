3c2b5b7a9e2eeaca5ca66996caf698d072cf6f89
railgun414 <414152672@qq.com>
2018-08-10 22:41:14 +0800

    修改UserService逻辑，使用mybatis example查询

--- src/main/java/com/citi/portfoliomanager/service/UserService.java
+++ src/main/java/com/citi/portfoliomanager/service/UserService.java
@@ -1,7 +1,9 @@
 package com.citi.portfoliomanager.service;
 
-import com.citi.portfoliomanager.dao.UserDao;
+
+import com.citi.portfoliomanager.dao.UserMapper;
 import com.citi.portfoliomanager.entity.User;
+import com.citi.portfoliomanager.entity.UserExample;
 import com.citi.portfoliomanager.service.IService.IUserService;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -18,10 +20,17 @@ public class UserService implements IUserService {
     protected static final Logger logger = LogManager.getLogger(UserService.class);
 
     @Autowired
-    private UserDao userDao;
+    private UserMapper userMapper;
 
     @Override
     public User getUser(String username) {
-        return userDao.get(username);
+    	UserExample userExample=new UserExample();
+    	userExample.createCriteria().andUsernameEqualTo(username);
+    	List<User> res=userMapper.selectByExample(userExample);
+    	if(res==null||res.size()<=0) {
+    		return null;
+    	}else {
+             return res.get(0);
+    	}
     }
 }
