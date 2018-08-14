bec80e35953f808b87017714780d592ee8caea2d
24320142202429 <24320142202429@stu.xmu.edu.cn>
2018-08-14 10:42:13 +0800

    ProductHistory implementation

--- src/test/java/com/citi/portfolio/service/PositionServiceTests.java
+++ src/test/java/com/citi/portfolio/service/PositionServiceTests.java
@@ -1,6 +1,7 @@
 package com.citi.portfolio.service;
 
 import com.citi.portfolio.AppTests;
+import com.citi.portfoliomanager.constant.SystemDate;
 import com.citi.portfoliomanager.service.IService.IPositionService;
 import com.citi.portfoliomanager.service.PositionService;
 import org.junit.Test;
@@ -17,6 +18,10 @@ public class PositionServiceTests extends AppTests {
 
     @Test
     public void buyProduct() throws Exception {
+        Date date= SystemDate.getSysDate();
+        if(positionService.buyProduct(3,date,"Nikkei",100))
+            System.out.println("success");
+        else System.out.println("product history not exit");
         //Date today=
         //positionService.buyProduct(1,)
     }
